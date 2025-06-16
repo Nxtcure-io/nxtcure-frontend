7eb367d4a39f670c56d58455e252aa749cd7d974
nxtcure-dev <dev@nxtcure.com>
2025-06-16 16:18:16 -0400

    testing next Q in FAQ

--- src/components/page/FAQSection.jsx
+++ src/components/page/FAQSection.jsx
@@ -24,6 +24,25 @@ export default function FAQSection() {
 	  	If you are eligible for several trials, NxtCure will provide you with an Eligibility Score for each one to help you prioritize based on fit, travel distance, and likelihood of acceptance. You can then choose which trial(s) to explore further. We’ll provide direct links to trial coordinators so you can contact them to start the screening process.
           </p>
         </motion.div>
+
+        <motion.div
+          className="w-full md:w-1/2 text-left mx-auto"
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false, amount: 0.3 }}
+          transition={{ delay: 0.2, duration: 0.8, ease: "easeOut" }}
+        >
+          <h2 className="cormorant text-2xl font-bold text-gray-800 mb-4">
+		2. How is my personal data protected?
+          </h2>
+          <p
+            className="text-gray-600 text-lg leading-relaxed"
+            style={{ fontSize: "1rem" }}
+          >
+	  Your privacy is our top priority. All uploaded electronic health records (EHRs) are anonymized and HIPAA-compliant. Personally identifiable information—such as names, addresses, and full birth dates—is removed before data is analyzed. Patients may upload de-identified records themselves or request our team to de-identify their documents securely. We do not sell or share your data with third parties.
+          </p>
+        </motion.div>
+
       </div>
     </section>
   );
