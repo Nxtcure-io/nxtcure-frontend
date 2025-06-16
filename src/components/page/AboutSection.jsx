513a78e5d441b5dcb5775b17ea4705d0041a76a9
nxtcure-dev <dev@nxtcure.com>
2025-06-16 14:53:24 -0400

    about page finished

--- src/components/page/AboutSection.jsx
+++ src/components/page/AboutSection.jsx
@@ -19,16 +19,16 @@ export default function AboutSection() {
             className="text-gray-600 text-lg leading-relaxed"
             style={{ fontSize: "1rem" }}
           >
-		NxtCure is the ultimate tool for clinical trials—empowering patients and physicians to navigate the complex world of research and treatment innovation.
+		<b>NxtCure</b> is the ultimate tool for clinical trials—empowering patients and physicians to navigate the complex world of research and treatment innovation.
 		<br />
 	  	<br />
-		Every year, millions of patients are interested in joining clinical trials, yet 73% report having no idea how to find them. At NxtCure, we help patients discover trials that match their unique medical profiles, while enabling physicians to assess the availability, necessity, and scientific rigor of clinical trials—especially when FDA-approved treatments already exist.
+		Every year, millions of patients are interested in joining clinical trials, yet <b>73%</b> report having no idea how to find them. At NxtCure, we help <b>patients</b> discover trials that match their unique medical profiles, while enabling <b>physicians</b> to assess the availability, necessity, and scientific rigor of clinical trials—especially when FDA-approved treatments already exist.
 	  	<br />
 	  	<br />
 		We’re built on the belief that today’s clinical trials are often too exclusive. Overly strict inclusion and exclusion criteria make recruitment difficult and leave many patients behind. As Kim et al. (BMC, 2020) noted, these narrow designs limit generalizability and underrepresented real-world populations. Bozkurt et al. (Lancet, 2022) found that out of 5.6 million U.S. adults eligible for heart failure treatment, only a small percentage qualified for trials due to restrictive eligibility requirements.
 	  	<br />
 	  	<br />
-		NxtCure is reimagining clinical trials to be more accessible, inclusive, and impactful—for the patients who need them and the physicians who guide their care.
+		<b>NxtCure</b> is reimagining clinical trials to be more accessible, inclusive, and impactful—for the patients who need them and the physicians who guide their care.
           </p>
         </motion.div>
       </div>
