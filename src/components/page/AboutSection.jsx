ab4c188efa8c276a17602d85b5c6ae5e6cf4edcf
nxtcure-dev <dev@nxtcure.com>
2025-06-16 14:27:35 -0400

    fixing file path for image

--- src/components/page/AboutSection.jsx
+++ src/components/page/AboutSection.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import about from "../assets/about.png";
+import about from "../../assets/about.png";
 
 export default function AboutSection() {
   return (
