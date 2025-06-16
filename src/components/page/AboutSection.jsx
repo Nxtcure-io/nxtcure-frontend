25eee8f36d1e8cde3ff462482ebcc620c5db8cc0
nxtcure-dev <dev@nxtcure.com>
2025-06-16 14:30:34 -0400

    updating content of about page

--- src/components/page/AboutSection.jsx
+++ src/components/page/AboutSection.jsx
@@ -12,39 +12,23 @@ export default function AboutSection() {
           viewport={{ once: false, amount: 0.3 }}
           transition={{ delay: 0.2, duration: 0.8, ease: "easeOut" }}
         >
-          <div
-            className="bg-white rounded-full text-gray-500 mb-4"
-            style={{
-              width: "fit-content",
-              fontSize: "0.7rem",
-              padding: "0.2rem 0.8rem 0.2rem 0.8rem",
-              border: "1px solid gray",
-              boxShadow: "1px 2px 5px gray",
-            }}
-          >
-            Our Story
-          </div>
           <h2 className="cormorant text-5xl font-bold text-gray-800 mb-4">
-            How It All Started?
+            About Us
           </h2>
           <p
             className="text-gray-600 text-lg leading-relaxed"
             style={{ fontSize: "1rem" }}
           >
-            This is Gonzalo—a survivor, a fighter, and a beacon of hope.
-            Gonzalo’s journey is extraordinary. He first underwent open-heart
-            surgery, faced 2 heart attacks, then faced kidney failure, 2
-            strokes, and gangrene. Today, he’s a double amputee and is looking
-            for a clinical trial.
-            <br />
-            <br />
-            Gonzalo is the reason we started NxtCure- to help patients like him
-            find clinical trials that offer hope, healing, and a path forward.
-            Gonzalo is currently look for a treatment for his heart condition
-            and his stroke. He and patients just like him was our motivation for
-            starting NxtCure- we believe everyone should have an opportunity for
-            treatment. NxtCure is here to guide patients like him every step of
-            the way.
+		NxtCure is the ultimate tool for clinical trials—empowering patients and physicians to navigate the complex world of research and treatment innovation.
+		<br />
+	  	<br />
+		Every year, millions of patients are interested in joining clinical trials, yet 73% report having no idea how to find them. At NxtCure, we help patients discover trials that match their unique medical profiles, while enabling physicians to assess the availability, necessity, and scientific rigor of clinical trials—especially when FDA-approved treatments already exist.
+	  	<br />
+	  	<br />
+		We’re built on the belief that today’s clinical trials are often too exclusive. Overly strict inclusion and exclusion criteria make recruitment difficult and leave many patients behind. As Kim et al. (BMC, 2020) noted, these narrow designs limit generalizability and underrepresented real-world populations. Bozkurt et al. (Lancet, 2022) found that out of 5.6 million U.S. adults eligible for heart failure treatment, only a small percentage qualified for trials due to restrictive eligibility requirements.
+	  	<br />
+	  	<br />
+		NxtCure is reimagining clinical trials to be more accessible, inclusive, and impactful—for the patients who need them and the physicians who guide their care.
           </p>
         </motion.div>
       </div>
