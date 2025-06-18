f943b28b61c63187df0daf959699a74624949811
nxtcure-dev <dev@nxtcure.com>
2025-06-18 17:16:44 -0400

    exporting waitlist

--- src/components/WaitlistAlert.jsx
+++ src/components/WaitlistAlert.jsx
@@ -2,7 +2,7 @@
 
 import { Alert } from "flowbite-react";
 
-export function WaitlistAlert() {
+export default function WaitlistAlert() {
   return (
     <Alert color="success" onDismiss={() => alert('Please fill out this form!')}>
    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
