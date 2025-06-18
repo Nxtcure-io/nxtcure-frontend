bc998659f738236b73f4631f3bfcbd79f9ed51bd
nxtcure-dev <dev@nxtcure.com>
2025-06-18 17:15:25 -0400

    resizing alert button

--- src/components/WaitlistAlert.jsx
+++ src/components/WaitlistAlert.jsx
@@ -4,9 +4,9 @@ import { Alert } from "flowbite-react";
 
 export function WaitlistAlert() {
   return (
-    <Alert color="success" onDismiss={() => alert('Alert dismissed!')}>
+    <Alert color="success" onDismiss={() => alert('Please fill out this form!')}>
    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
-      <span className="font-medium">Join the Waitlist</span> 
+      <span className="font-medium h-[10rem]">Join the Waitlist</span> 
 	 </a>
     </Alert>
   );
