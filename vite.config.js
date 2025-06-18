f01cc3c283d673717e5cc318ca4933faa0eb189b
nxtcure-dev <dev@nxtcure.com>
2025-06-18 17:32:59 -0400

    adding alert deps

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   // eslint-disable-next-line no-undef
   base: process.env.VITE_BASE_PATH || "/nxtcure-frontend",
 });
