4a00a0f003ad67ed539105b84b4e3deabec51c82
nxtcure-dev <dev@nxtcure.com>
2025-06-18 17:23:10 -0400

    styling alert

--- src/components/WaitlistAlert.jsx
+++ src/components/WaitlistAlert.jsx
@@ -5,8 +5,8 @@ import { Alert } from "flowbite-react";
 export default function WaitlistAlert() {
   return (
     <Alert color="success" onDismiss={() => alert('Please fill out this form!')}>
-   <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
-      <span className="font-medium text-center text-6xl justify-center">Join the Waitlist</span> 
+   <a className="block text-center" href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
+      <span className="font-medium text-6xl">Join the Waitlist</span> 
 	 </a>
     </Alert>
   );
