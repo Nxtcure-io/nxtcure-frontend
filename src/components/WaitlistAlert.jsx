d787f9111c72de7ba8665d1f4cb859e902e37786
nxtcure-dev <dev@nxtcure.com>
2025-06-18 17:18:46 -0400

    resizing waitlist maybe

--- src/components/WaitlistAlert.jsx
+++ src/components/WaitlistAlert.jsx
@@ -6,7 +6,7 @@ export default function WaitlistAlert() {
   return (
     <Alert color="success" onDismiss={() => alert('Please fill out this form!')}>
    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
-      <span className="font-medium h-[10rem]">Join the Waitlist</span> 
+      <span className="font-medium text-center text-3xl">Join the Waitlist</span> 
 	 </a>
     </Alert>
   );
